8bd79584cb052d55c6bc476ad10f6829791eafc6
XiaoHai-Smida <1951565329@qq.com>
2023-05-28 22:31:25 +0800

    更新频域能量的统计分析

--- sts.m
+++ sts.m
@@ -5,6 +5,7 @@ close all
 load ./processed/psd_for_sub_freq.mat
 [cha,sub,freq] = size(psd0_for_sub_freq);
 
+
 for f = 1:freq
     for c = 1:cha
         x = psd0_for_sub_freq(c,:,f);
@@ -12,14 +13,14 @@ for f = 1:freq
             % 离群值检测
         x = filloutliers(x,'spline','quartiles');
         y = filloutliers(y,'spline','quartiles');
-        
+        % 配对T检验
         [h(c,f),p(c,f)] = ttest(x,y);
     end    
 end
 
 
 
-
-for f = 1:freq
-    fdr(:,f) = mafdr(p(:,f),'BHFDR',true);
-end
+% 
+% for f = 1:freq
+%     fdr(:,f) = mafdr(p(:,f),'BHFDR',true);
+% end
